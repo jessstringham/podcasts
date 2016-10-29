33dcc93215db3de6903f224ed84d4b99b5c41d03
Jessica Stringham <jessstringham@users.noreply.github.com>
2016-10-28 19:05:57 -0700

    add readme and license

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     package_data={
-        '': ['LICENSE', 'README.md', 'RELEASE']
+        '': ['LICENSE', 'README.md']
     },
 )
